03c76ec71d7fe79cb2fd618ab3d9cb727216a6af
Jessica Brumfield <jessicabrumfield@Jessicas-MacBook-Pro.local>
2019-05-19 15:10:50 -0500

    added fb button to app, deleted fb button file

--- project-frontend/HealthWe2/App.js
+++ project-frontend/HealthWe2/App.js
@@ -1,16 +1,41 @@
 
 import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, Text, View} from 'react-native';
-
-var FBLoginButton = require('./PageComponents/FBLoginButton');
+import {AppRegistry, StyleSheet, Text, View, Button,Alert} from 'react-native';
+import {Facebook} from 'expo'
 
 
 export default class App extends Component {
+  state={
+       fbInfo:[]
+  }
   render() {
+    async function logIn() {
+      try {
+        const {
+          type,
+          token,
+          expires,
+          permissions,
+          declinedPermissions,
+        } = await Facebook.logInWithReadPermissionsAsync('418683202019231', {
+          permissions: ['public_profile'],
+        });
+        if (type === 'success') {
+          // Get the user's name using Facebook's Graph API
+          const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+          Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+        } else {
+          // type === 'cancel'
+        }
+      } catch ({ message }) {
+        alert(`Facebook Login Error: ${message}`);
+      }
+    }
+    console.log(this.state.fbInfo)
     return (
       <View style={styles.container}>
-        <Text style={styles.label}>Welcome to the Facebook SDK for React Native!</Text>
-        <FBLoginButton />
+        <Text style={styles.label}>Welcome to HealthWe!</Text>
+        <Button onPress={logIn} title='link your facebook'></Button>
 
       </View>
     );
